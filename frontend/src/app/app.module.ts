919d4ec342e9a29475cb6f1d7fded4bf7bc40ecf
VitorJobs <vittorjobs@gmail.com>
2022-07-06 11:42:00 -0300

    :construction: feat: add nav component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,23 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
 
-/**Material Design ToolBar - Header */
+/** COMPONENTS*/
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component'
+
+/**Material Design ToolBar - Header, Footer, Nav */
 import { MatToolbarModule} from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component'
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
