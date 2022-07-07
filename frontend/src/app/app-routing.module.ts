3af86fec408a8fbd9daae8620c3685c6b74cef6e
VitorJobs <vittorjobs@gmail.com>
2022-07-07 16:26:23 -0300

    :soon: feat: Routs home and products-crud

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// COMPONENTES DE NAVEGAÇÃO 
+// VIEWS {
+//   home
+//   product-crud
+// }
+
+import {HomeComponent} from "./views/home/home.component"
+import {ProductCrudComponent} from "./views/product-crud/product-crud.component"
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
