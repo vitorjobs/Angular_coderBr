4614e85b08cda14780e2bf14d8a2640d56b58acd
VitorJobs <vittorjobs@gmail.com>
2022-07-07 17:32:47 -0300

    :zap: chore: implementes component product-create

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {HomeComponent} from "./views/home/home.component"
 import {ProductCrudComponent} from "./views/product-crud/product-crud.component"
+import {ProductCreateComponent} from './components/template/product/product-create/product-create.component';
+
 
 const routes: Routes = [
   {
@@ -18,6 +20,10 @@ const routes: Routes = [
   {
     path: "products",
     component: ProductCrudComponent
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent
   }
 ];
 
